2466d081e03e3b558b1b3a57e65ab46b5467b65e
Danijel Arsenovski <danijiel.arsenovski@alaskaair.com>
2015-11-18 09:35:07 -0800

    Updated to Visual Studio 2015. Visual Basic koans temporarily disabled.

---
+++ CSharp/AboutOperators.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetKoans.CSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public class AboutOperators : Koan
+    {
+        [Koan(1)]
+        public void ElvisOperator()
+        {
+            string foo = null;
+            try
+            {
+                foo = foo?.ToLower();
+            }
+            catch (NullReferenceException ex)
+            {
+                foo = "exception";
+            }
+            Assert.Equal(FILL_ME_IN, foo);            
+        }
+    }
+}
