722fc7c4f6aa9bf1c836ad3e66e9da957a5ba3ba
darsen <danijiel.arsenovski@alaskaair.com>
2016-04-04 16:45:10 -0700

    More extension method koans

--- CSharp/AboutExtensionMethods.cs
+++ CSharp/AboutExtensionMethods.cs
@@ -6,9 +6,6 @@
 
     public class AboutExtensionMethods : Koan
     {
-        private readonly int __ = 0;
-        private readonly string ___ = "";
-
         [Koan(1)]
         public void Filter()
         {
@@ -27,7 +24,7 @@
                 new List<string> {"Homage to Catalonia", "Burmese Days"}
             };
             var all = fiction_and_nonfiction.SelectMany(b => b).ToList();
-            Assert.Equal(all, new List<string> {___, ___, ___, ___});
+            Assert.Equal(all, FILL_ME_IN);
         }
 
         [Koan(3)]
@@ -35,7 +32,7 @@
         {
             var books = new List<string> {"1984", "Animal Farm", "Homage to Catalonia", "Burmese Days"};
             var title_length = books.Select(t => t.Length);
-            Assert.Equal(title_length, new[] {__, __, __, __});
+            Assert.Equal(title_length, FILL_ME_IN);
         }
 
         [Koan(4)]
@@ -44,7 +41,25 @@
             var Orwells = new List<string> {"1984", "Animal Farm", "Homage to Catalonia", "Burmese Days"};
             var great_novels = new List<string> {"Hear of Darkness", "1984", "Animal Farm", "Ulysses"};
             var Orwells_great_novels = great_novels.Intersect(Orwells).ToArray();
-            Assert.Equal(Orwells_great_novels, new[] { ___, ___ });
+            Assert.Equal(Orwells_great_novels, FILL_ME_IN);
         }
+
+        [Koan(5)]
+        public void Union()
+        {
+            var Orwells = new List<string> { "1984", "Animal Farm", "Homage to Catalonia", "Burmese Days" };
+            var great_novels = new List<string> { "Hear of Darkness", "1984", "Animal Farm", "Ulysses" };
+            var all = great_novels.Union(Orwells).ToArray();
+            Assert.Equal(all, FILL_ME_IN);
+        }
+
+        [Koan(6)]
+        public void Aggregate()
+        {
+            var books = new List<string> { "1984", "Animal Farm", "Homage to Catalonia", "Burmese Days" };
+            var more_books = books.Aggregate("Orwell's novels are: ", (a, b) => a + b + ", ") + "A Clergyman's daughter";
+            Assert.Equal(more_books, FILL_ME_IN);
+        }
+
     }
 }
